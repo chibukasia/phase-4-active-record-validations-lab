e6cde7af94bd410d147a58f535e8d507df69dc92
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-09-29 14:16:26 +0300

    commit tree 42ae2a377f56a843034f6519504aadfdae4ef974

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :is_valid_title
+    # custom validations 
+    def is_valid_title
+        words = ["Won't Believe","Secret","Top [number]","Guess"]
+        unless words.any? {|word| if title && !title.match?(word)}
+            errors.add(:title, "Does not contain one of the above")
+        end
+    end
 end
